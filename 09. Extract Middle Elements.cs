3bcb721049e78e07a90e944672dfa7c2f96cfb48
viewless <nikola.dionisiev@gmail.com>
2019-10-04 06:02:34 +0300

    viewless

--- 09. Extract Middle Elements.cs
+++ 09. Extract Middle Elements.cs
@@ -7,50 +7,24 @@ namespace _09._Extract_Middle_Elements
     {
         static void Main(string[] args)
         {
-            long[] nums = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
-            long[] midElements = MidElementsOfArray(nums);
-            PrintArr(midElements);
-        }
+            string input = Console.ReadLine();
 
-        static long[] MidElementsOfArray(long[] arr)
-        {
-            int n = arr.Length;
-            long[] newArr;
+            Stack<int> expressionFinder = new Stack<int>(input.Length);
 
-            if (arr.Length == 1)
+            for (int i = 0; i < input.Length; i++)
             {
-                newArr = new long[1];
-                newArr[0] = arr[0];
-            }
-            else if (arr.Length % 2 == 0)
-            {
-                newArr = new long[2];
-                newArr[0] = arr[n / 2 - 1];
-                newArr[1] = arr[n / 2];
-            }
-            else
-            {
-                newArr = new long[3];
-                newArr[0] = arr[n / 2 - 1];
-                newArr[1] = arr[n / 2];
-                newArr[2] = arr[n / 2 + 1];
-            }
-
-            return newArr;
-        }
-
-        static void PrintArr(long[] arr)
-        {
-            Console.Write("{ ");
-            foreach (long item in arr)
-            {
-                Console.Write(item);
-                if (!item.Equals(arr[arr.Length - 1]))
+                if (input[i] == '(')
                 {
-                    Console.Write(", ");
+                    expressionFinder.Push(i);
                 }
+
+                if (input[i] == ')')
+                {
+                    int start = expressionFinder.Pop();
+                    Console.WriteLine(input.Substring(start, i - start + 1));
+                }
+
             }
-            Console.Write(" }");
         }
     }
 }
